a9cb473f04082d8120914f063f767b61f249f5bd
Mare-laptop <Mare-laptop@MARE-LAPTOP-PC>
2018-06-16 20:25:48 +0200

    Seminarska naloga spletna aplikacija

---
+++ MVCNajprejKoda/Controllers/JsonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SkladisceFilmov.Models;
+
+namespace SkladisceFilmov.Controllers
+{
+    public class JsonController : ApiController
+    {
+        private FilmiContext db = new FilmiContext();
+
+        // GET: api/Json
+        public IQueryable<Film> GetFilms()
+        {
+            return db.Films;
+        }
+
+        
+    }
+}
---
+++ dbo.Films.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Films] (
+    [id]         INT             IDENTITY (1, 1) NOT NULL,
+    [Naslov]     NVARCHAR (MAX)  NULL,
+    [LetoIzdaje] DATETIME        NOT NULL,
+    [Tip]        NVARCHAR (MAX)  NULL,
+    [Cena]       DECIMAL (18, 2) NOT NULL,
+    CONSTRAINT [PK_dbo.Films] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
